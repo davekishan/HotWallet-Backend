203f290c959f872ae34c8e0a05bb8274926ca46d
davekishan <75985572+davekishan@users.noreply.github.com>
2023-06-23 12:50:00 +0530

    create mathod

--- src/controller/wallet.js
+++ src/controller/wallet.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const Web3 = require("web3");
 const userWallet = require("../module/wallet");
+const userModel = require('../module/Usermodel');
 const apikey = process.env["apiKey"];
 // console.log(apikey)
 // const network  =  'goerli';
@@ -14,14 +15,6 @@ const web3 = new Web3(node);
 
 // Create Random account address
 let account1;
-const createWallet = async () => {
-  const accountTo = web3.eth.accounts.create();
-  console.log(accountTo);
-  console.log("This is New Wallet Address:", accountTo.address);
-  console.log("Here is your PrivateKey:", accountTo.privateKey);
-  account1 = await accountTo;
-  return accountTo;
-};
 
 // const abc = async() => {
 //     const a = await createWallet();
@@ -32,9 +25,6 @@ const createWallet = async () => {
 const privateKey = process.env.privateKey;
 // console.log("Here is your private key:",privateKey);
 
-const create_wallet = (email) => {
-  return createWallet(email);
-};
 const accountFrom = web3.eth.accounts.privateKeyToAccount(privateKey);
 
 const amountTo = "0.00001"; //ether amount
@@ -70,9 +60,82 @@ const createSignedTx = async (account, email) => {
   }
 };
 
-const callfun = (account, email) => {
-  createSignedTx(account, email);
-};
+
+const deposite = async (account, amount, email) => {
+  const rawTx1 = {
+    to: account,
+    value: web3.utils.toWei(amount, "ether"),
+  }
+
+  const estimatefees = web3.eth.estimateGas(rawTx1)
+
+  const rawTx = {
+    to: account,
+    value: web3.utils.toWei(amount, "ether"),
+    gas: estimatefees,
+
+
+  }  // sign transaction 
+ 
+
+    const signedtx = await web3.eth.accounts.signTransaction(rawTx, privateKey);   // sign transaction 
+    const receipt = await web3.eth.sendSignedTransaction(signedtx.rawTransaction); // send signed transaction
+    await userWallet.findOneAndUpdate({ email: email, walletAddress: account }, { '$inc': { 'balance': + amount } })
+    console.log(receipt);
+    console.log("Done");
+
+    // console.log("This is error");
+    console.log(error);
+    return "Error"
+
+}
+
+
+
+const createWallet = (email) => {
+  const accountTo = web3.eth.accounts.create();
+  console.log(accountTo)
+  const userwallet = userWallet({ email: email, walletAddress: accountTo.address, privatekey: accountTo.privateKey, balance: 0 })
+  userwallet.save();
+  return true;
+}
+
+
+const sendeth = async (from, to, value1, email) => {
+  const account = await userWallet.findOne({ email: email, walletAddress: from })
+  const value = web3.utils.toWei(value1, "ether")
+  const rawTx1 = {
+    to: to,
+    value: value
+  }
+
+  const estimatefees = await web3.eth.estimateGas(rawTx1)
+
+  const rawTx = {
+
+    to: to,
+    value: value,
+    gas: estimatefees,
+  }
+  console.log("Fees is :", estimatefees, "value is :", value);
+  console.log(parseInt(estimatefees) + parseInt(value));
+
+  console.log(account)
+ 
+  if (web3.utils.toWei((account.balance).toString(), "ether") > (parseInt(estimatefees) + parseInt(value))) {
+    
+    const signedtx = await web3.eth.accounts.signTransaction(rawTx, account.privatekey); 
+    const receipt = await web3.eth.sendSignedTransaction(signedtx.rawTransaction); // send signed transaction
+    console.log("receipt")  // sign transaction 
+
+    await userWallet.findOneAndUpdate({ email: email, walletAdddress: account }, { '$inc': { 'balance': - value } })
+    console.log(receipt);
+    console.log("Done");
+  }
+  else {
+    console.log("Balance is low");
+  }
+}
 
 // abc();
-module.exports = { createSignedTx, callfun, create_wallet };
+module.exports = { createWallet, deposite, sendeth };
