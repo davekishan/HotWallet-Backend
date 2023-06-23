b182c67f24accb9ae592550a40ef8318926a002a
davekishan <75985572+davekishan@users.noreply.github.com>
2023-06-23 12:50:27 +0530

    change sendeth,create

--- src/router/walletrouter.js
+++ src/router/walletrouter.js
@@ -6,36 +6,39 @@ const app = express();
 const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-const { callfun,  create_wallet } = require('../controller/wallet');
 const { createWallet,deposite,sendeth } = require('../controller/wallet');
 const userWallet = require('../module/wallet');
 dotenv.config();
 
-let account;
 app.use(express.json());
 app.use(cors());
 
 
 const walletrouter = express.Router();
-walletrouter.get('/sendeth',async (req, res) => {
-    email = req.session.email
-  
-   console.log("wallet router response");
-   const fun= callfun(account,email);
-  //  console.log(fun);
-   res.json({success:true})
-   
-  })
+
 walletrouter.get('/createwallet',async (req, res) => {
-    email = req.session.email
-  
-   console.log("wallet router response");
-   const fun= await create_wallet(email);
-   account =await fun;
-   console.log("VG");
+   const fun=await createWallet(req.session.email);
    console.log(fun);
-   res.json({success:true})
-   
+   res.json({success:true,message:"Account Created.."})
   })
-//test
+
+  
+walletrouter.post('/deposite',async (req, res) => {
+  const {account,value}=req.body;
+  const fun=await deposite(account,value,req.session.email);
+  console.log(fun);
+  res.json({success:true,message:"Deposite Ether...."})
+ })
+
+ 
+walletrouter.post('/sendeth',async (req, res) => {
+  const {account,value}=req.body;
+  const user=await userWallet.findOne({email:req.session.email})
+  console.log(req.session.email);
+  console.log(user);
+  const fun=await sendeth(user.walletAddress,account,value,req.session.email);
+  
+  res.json({success:true,message:"Send Ether Successfully..."})
+ })
+
 module.exports = walletrouter;
